fac9a874bfd1e05218cadc15d02b9b29ad40d52d
Shittu Idris <shittuidris45@yahoo.com>
2017-06-17 10:34:47 +0000

    Working Commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser= require('body-parser')
 var GoogleSpreadsheet = require('google-spreadsheet');
 var creds = require('./login-secret.json');
-var score = 0;
+var mark=0;
 
 
 
@@ -25,9 +25,7 @@ app.get('/', function(req, res) {
   	doc.useServiceAccountAuth(creds, function (err) {
   	// Get all of the rows from the spreadsheet.
   	doc.getRows(1, function (err, rows) {
-    //console.log(rows);
-    // console.log(rows[0].optiona);
-    //renders index.ejs
+    
     res.render('index.ejs', {data: rows})
   		});
 	});
@@ -38,39 +36,41 @@ app.get('/', function(req, res) {
 
 app.post('/engine', function(req, res) {
 
-   
+  
   	
   	// Create a document object using the ID of the spreadsheet - obtained from its URL.
 	var doc = new GoogleSpreadsheet('16p10gllBu5vEFlHODUvVXLlW8OVrFtD5kKM__krRY0Q');
 	// Authenticate with the Google Spreadsheets API.
   	doc.useServiceAccountAuth(creds, function (err) {
   	// Get all of the rows from the spreadsheet.
-  	doc.getRows(1, function (err, rows) {
-	for(var i=0; i < rows.length; i++) {
-		if((rows[i].correctanswer)==(req.body[i])) score++;
-	}
-    
-   
-   // res.render('result.ejs', '')
-  		});
+
+  	  doc.getRows(1, function (err, rows) {
+	    
+		for(var i=0; i < rows.length; i++) {
+			if((rows[i].correctanswer)==(req.body[i])) mark= mark + 1;
+		}
+
+
+		var docwrt = new GoogleSpreadsheet('1gqGypYQiGIpiS1-8LZLGtPLDwMNaoBSklRS2YOMh6Q8');
+	    // Authenticate with the Google Spreadsheets API.
+	    docwrt.useServiceAccountAuth(creds, function (err) {
+		
+		  docwrt.addRow(1, { result: mark }, function(err) {
+		
+  		    if(err) {
+    		  console.log(err);
+ 		    }
+		  });
+		  
+		  res.render('result.ejs',{score:mark})
+		  mark=0;
+
+	    });
+
+  	   });
+       
 	});
 
-
-	var docwrt = new GoogleSpreadsheet('1gqGypYQiGIpiS1-8LZLGtPLDwMNaoBSklRS2YOMh6Q8');
- 
-	// Authenticate with the Google Spreadsheets API.
-	docwrt.useServiceAccountAuth(creds, function (err) {
-		docwrt.addRow(1, { result: score }, function(err) {
-  		if(err) {
-    		console.log(err);
- 		 }
-		});
- 
-	});
-
-	console.log(score);
-
-  
 })
 
 
